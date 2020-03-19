bcb102f802b5324b1a13b039101c4d44c0d8905d
Lucas França <luc.rangel90@gmail.com>
2020-03-18 22:47:53 -0300

    nda

---
+++ exercises/6_1/script.js
@@ -0,0 +1,38 @@
+let estados = [
+    'Selecione',
+    'Acre',
+    'Alagoas',
+    'Amapá',
+    'Amazonas',
+    'Bahia',
+    'Ceará',
+    'Distrito Federal',
+    'Espírito Santo',
+    'Goiás',
+    'Maranhão',
+    'Mato Grosso',
+    'Mato Grosso do Sul',
+    'Minas Gerais ',
+    'Pará',
+    'Paraíba',
+    'Paraná',
+    'Pernambuco',
+    'Piauí',
+    'Rio de Janeiro',
+    'Rio Grande do Norte',
+    'Rio Grande do Sul',
+    'Rondônia',
+    'Roraima',
+    'Santa Catarina',
+    'São Paulo',
+    'Sergipe',
+    'Tocantins'
+  ];
+  
+  let selecioneEstados = document.querySelector('#estadosBrasileiros');
+  
+  for (i = 0; i < estados.length; i++) {
+    let options = document.createElement('option');
+    options.text = estados[i];
+    selecioneEstados.appendChild(options);
+  }
