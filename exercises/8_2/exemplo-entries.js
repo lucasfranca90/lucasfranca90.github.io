8e3eda0270af40448e0b14773c81c4702e7ecaa5
Lucas Franca <luc.rangel90@gmail.com>
2020-04-10 19:02:27 -0300

    Add exemplos

---
+++ exercises/8_2/exemplo-entries.js
@@ -0,0 +1,16 @@
+// Exemplo de uso de entries.
+
+const países = {
+  França: 'Paris',
+  Brasil: 'Brasília',
+  Espanha: 'Madrid',
+  Portugal: 'Lisboa',
+};
+const pairKeyValue = Object.entries(países);
+console.log(pairKeyValue);
+
+for(i in pairKeyValue) {
+  console.log('--------');
+  console.log('Pais:', pairKeyValue[i][0]);
+  console.log('Capital:', pairKeyValue[i][1]);
+};
