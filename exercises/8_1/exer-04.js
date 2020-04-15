285deee8f596de0e0cd3a49b12a243de6b3fd2c4
Lucas Franca <luc.rangel90@gmail.com>
2020-04-14 23:52:47 -0300

    exercicio 4

--- exercises/8_1/exer-04.js
+++ exercises/8_1/exer-04.js
@@ -13,3 +13,32 @@ Tudo bem?“.
 Um array com escopo global, que é o escopo do arquivo JS nesse caso, contendo cinco strings com suas principais skills.
 
 Função 2: Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à string que foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string.*/
+
+const array = ["Android", "iOS", "Architecture", "Teach", "Run"]
+
+function buildSkillsPhrase (paramOne) {
+    const fun1 = paramInner => (
+      `Tryber ${paramInner} aqui!
+
+      Tudo bem?`
+    )
+
+    let result = `${fun1(paramOne)}
+
+    Minhas cinco principais habilidades são:`
+
+    array.forEach((skill, index) =>
+    result = `${result}
+
+    - ${skill}`)
+
+    result = `
+{result}
+
+    #goTrybe
+    `
+
+    return result
+}
+
+console.log(buildSkillsPhrase("Lucas"))
