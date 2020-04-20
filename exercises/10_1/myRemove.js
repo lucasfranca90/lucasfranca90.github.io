01333f1cebb6ec34ad14905b7bfdec7d37830b30
Lucas Franca <luc.rangel90@gmail.com>
2020-04-20 00:18:50 -0300

    dependencias

--- exercises/10_1/myRemove.js
+++ exercises/10_1/myRemove.js
@@ -1,4 +1,5 @@
-// myRemove.js
+// A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array.
+
 const myRemove = (arr, item) => arr.filter((element) => element !== item)
 
 module.exports = myRemove;
--- exercises/10_1/myRemove.test.js
+++ exercises/10_1/myRemove.test.js
@@ -1,4 +1,8 @@
-// myRemove.test.js
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+// Faça uma chamada para a função myRemove e verifique que o array passado por parâmetro não sofreu alterações
+// Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+
 const myRemove = require('./myRemove');
 
 describe('myRemove', () => {
